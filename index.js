77037f4f31ed1feb6241bcd60ce5e800851f65dc
Kevin Randles <krandles@gmail.com>
2017-11-03 22:30:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -152,6 +152,12 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   window.requestAnimationFrame(function() {
+     var left = positionToInteger(DODGER.style.left)
+     if (left > 0) {
+       DODGER.style.left = `${left - 4}px`
+     }
+   })   
 }
 
 function moveDodgerRight() {
@@ -160,6 +166,12 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   window.requestAnimationFrame(function() {
+    var left = positionToInteger(DODGER.style.left)
+    if (left < 360) {
+      DODGER.style.left = `${left + 4}px`
+    }
+  })
 }
 
 /**
